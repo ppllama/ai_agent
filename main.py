ca28c999cd1a9e49c24c011d38337e436a2254ee
ppllama <neelapanchakan@gmail.com>
2025-06-15 20:11:55 +0530

    ch3l2

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from config import system_prompt
+from functions.get_files_info import schema_get_files_info
+
 
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("AI Code Assistant")
@@ -16,8 +19,15 @@ def main():
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    user_prompt = args[0]
+    user_prompt = " ".join(args)
 
+    
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )   
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -29,15 +39,21 @@ def main():
     response = client.models.generate_content(
         model = 'gemini-2.0-flash-001',
         contents = messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
     )
 
-    print(response.text)
-    if len(args) >= 2:
-        if args[1] == "--verbose":
-            print("User prompt:", user_prompt)
-            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-            print("Response tokens:", response.usage_metadata.candidates_token_count)
+    function_calls = response.function_calls
+    if function_calls != []:
+        for call in function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+    else:
+        print(response.text)
+
+
+    if verbose:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
